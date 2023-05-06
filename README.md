e63d5500ad6b0c608deb0b09c8ceab54ac748cf3
ethanlee928 <ethan2000.el@gmail.com>
2023-05-06 19:32:05 +0800

    updated README and added setup for pypi

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 - An easy-to-use python package to do Monte-Carlo Simulation on stock prices
 - GPU accelerated Monte-Carlo simulation, that could allow simulation more random walkers without a large time penalty
 
-## Get Started
+## For Development
 
 Python virtual environment:
 
---
+++ setup.py
@@ -0,0 +1,22 @@
+import os
+from setuptools import setup, find_packages
+
+
+setup(
+    name="pyfmc",
+    version=os.getenv("VER", None),
+    author="Ethan Lee",
+    author_email="ethan2000.el@gmail.com",
+    description="Finance Monte-Carlo Simulation using PyTorch",
+    long_description=open("README.md", "r").read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages("pyfmc"),
+    package_dir={"": "pyfmc"},
+    url="https://github.com/ethanlee928/pyfmc",
+    install_requires=["torch==2.0.0", "tqdm==4.65.0", "numpy==1.24.3", "pandas==2.0.0"],
+    classifiers=[
+        "Programming Language :: Python :: 3.8",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
