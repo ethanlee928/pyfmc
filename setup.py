5654f682a7e9cb63224836a137c24638cf12daf3
ethanlee928 <ethan2000.el@gmail.com>
2023-05-07 15:33:10 +0800

    fixing setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 setup(
     name="pyfmc",
-    version="0.0.3",
+    version="0.0.4",
     author="Ethan Lee",
     author_email="ethan2000.el@gmail.com",
     description="Finance Monte-Carlo Simulation using PyTorch",
     long_description=open("README.md", "r").read(),
     long_description_content_type="text/markdown",
+    packages=find_packages(),
+    package_dir={"": "pyfmc"},
     url="https://github.com/ethanlee928/pyfmc",
-    download_url="https://github.com/ethanlee928/pyfmc/archive/refs/tags/v0.0.3.tar.gz",
     install_requires=["torch==2.0.0", "tqdm==4.65.0", "numpy==1.24.3", "pandas==2.0.0"],
     classifiers=[
         "Programming Language :: Python :: 3.8",
