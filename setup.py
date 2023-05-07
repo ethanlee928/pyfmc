7da60d0b54b33e8fa26d495f859440445c3e3245
ethanlee928 <ethan2000.el@gmail.com>
2023-05-07 15:13:28 +0800

    minor update on setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyfmc",
-    version=os.environ["VER"],
+    version="0.0.2",
     author="Ethan Lee",
     author_email="ethan2000.el@gmail.com",
     description="Finance Monte-Carlo Simulation using PyTorch",
@@ -12,7 +12,7 @@ setup(
     long_description_content_type="text/markdown",
     packages=find_packages("pyfmc"),
     package_dir={"": "pyfmc"},
-    url=os.environ["URL"],
+    url="https://github.com/ethanlee928/pyfmc/archive/refs/tags/v0.0.2.tar.gz",
     install_requires=["torch==2.0.0", "tqdm==4.65.0", "numpy==1.24.3", "pandas==2.0.0"],
     classifiers=[
         "Programming Language :: Python :: 3.8",
