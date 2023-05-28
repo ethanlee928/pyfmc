73b41739896228d5d66c3eb545372651e90c8f78
ethanlee928 <ethan2000.el@gmail.com>
2023-05-28 17:12:24 +0800

    quick fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="pyfmc",
-    version="0.1.0",
+    version="0.1.1",
     author="Ethan Lee",
     author_email="ethan2000.el@gmail.com",
     description="Finance Monte-Carlo Simulation using PyTorch",
